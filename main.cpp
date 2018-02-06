469b7a28af8882bc7d48c0c36fbd6510614abf36
Zenkin <zenkinartem1997@gmail.com>
2018-02-06 23:13:59 +0300

    rubbish was removed

--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
-#include "ControllerNode.h"
 #include "mainwindow.h"
+#include <QApplication>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
 
+    ros::init(argc, argv, "controller_node_cpp");
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Window window(argc, argv);
+    window.setGeometry(400, 400, 200, 100);
+    window.show();
 
     return a.exec();
 
-//    std::cout << "start" << std::endl;
-//    ros::init(argc, argv, "controller_node_cpp");
-//    ControllerNode node;
-//    for(int i = 0; i < 2; i++) {
-//        node.MoveTo(2, 2);
-//        node.Control();
-//        node.MoveTo(-2, 2);
-//        node.Control();
-//        node.MoveTo(-2, -2);
-//        node.Control();
-//        node.MoveTo(2, -2);
-//        node.Control();
-//    }
-//    node.Hover();
-//    return 0;
 }
 
