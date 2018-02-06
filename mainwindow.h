203086557274159b5f0f508f5ba5d05bfba3bc39
Zenkin <zenkinartem1997@gmail.com>
2018-02-06 23:03:37 +0300

    MoveTo interface was added

--- mainwindow.h
+++ mainwindow.h
@@ -1,24 +1,58 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef WINDOW_H
+#define WINDOW_H
 
-#include "mainwindow.h"
-#include <QApplication>
-#include <QMainWindow>
+#include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QLineEdit>
+#include "drone.h"
+#include "ControllerNode.h"
 
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow
+class Window : public QDialog
 {
-    Q_OBJECT
+    Q_OBJECT //макрос, который дает понять, что будет обработан мок для сигналов и слотов
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+
+    Window(int argc, char** argv, QWidget *parent=0);
 
 private:
-    Ui::MainWindow *ui;
+
+    QLabel *lbl;
+    QLabel *navdata;
+    QLabel *x;
+    QLabel *y;
+
+    QPushButton *button_forward;
+    QPushButton *button_backward;
+    QPushButton *button_hover;
+    QPushButton *button_reset;
+    QPushButton *button_takeoff;
+    QPushButton *button_left;
+    QPushButton *button_right;
+    QPushButton *button_land;
+    QPushButton *button_moveto;
+
+    QLineEdit *x_field;
+    QLineEdit *y_field;
+
+private Q_SLOTS:
+
+    void ForwardIsClicked();
+    void BackwardIsClicked();
+    void HoverIsClicked();
+    void ResetIsClicked();
+    void TakeoffIsClicked();
+    void LeftIsClicked();
+    void RightIsClicked();
+    void LandIsClicked();
+    void MovetoIsClicked();
+
+private: 
+    ControllerNode node;
+    Drone drone;
 };
 
-#endif // MAINWINDOW_H
+#endif // WINDOW_H
